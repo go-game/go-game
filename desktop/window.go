503c492662dd537b336e50835d7b54051fd5bc3d
Malte Büchmann <malte.buechmann@wlw.de>
2016-08-24 08:13:31 +0200

    Fix closing of desktop window with panic

--- desktop/window.go
+++ desktop/window.go
@@ -1,10 +1,10 @@
 package desktop
 
 import (
-  "time"
-  "github.com/go-gl/glfw/v3.1/glfw"
-  "github.com/go-gl/gl/v2.1/gl"
-  "git.mbuechmann.com/go-game/game"
+	"git.mbuechmann.com/go-game/game"
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
+	"time"
 )
 
 // CurrentWindow is the current window
@@ -12,94 +12,93 @@ var CurrentWindow *Window
 
 // Mode represents the resolution of a window
 type Mode struct {
-  Width int
-  Height int
-  Fullscreen bool
+	Width      int
+	Height     int
+	Fullscreen bool
 }
 
 // Window is the os application frame where all the stuff will  happen
 type Window struct {
-  mode *Mode
-  state *game.State
-  GlfwWindow *glfw.Window
+	mode       *Mode
+	state      *game.State
+	GlfwWindow *glfw.Window
 }
 
 // FullscreenModes returns an array of all available fullscreen modes
 func FullscreenModes() []*Mode {
-  modes := []*Mode{}
+	modes := []*Mode{}
 
-  monitor := glfw.GetPrimaryMonitor()
-  videoModes := monitor.GetVideoModes()
-  for _, m := range(videoModes) {
-    modes = append(modes, &Mode{Width: m.Width, Height: m.Height, Fullscreen: true})
-  }
+	monitor := glfw.GetPrimaryMonitor()
+	videoModes := monitor.GetVideoModes()
+	for _, m := range videoModes {
+		modes = append(modes, &Mode{Width: m.Width, Height: m.Height, Fullscreen: true})
+	}
 
-  return modes
+	return modes
 }
 
 // OpenWindow creates a new window on the main monitor
 func OpenWindow(m *Mode, s *game.State) *Window {
-  err := glfw.Init()
-  if err != nil {
-    panic(err)
-  }
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
 
-  CurrentWindow = &Window{mode: m, state: s}
+	CurrentWindow = &Window{mode: m, state: s}
 
-  err = CurrentWindow.initGlfwWindow()
-  if err != nil {
-    panic(err)
-  }
+	err = CurrentWindow.initGlfwWindow()
+	if err != nil {
+		panic(err)
+	}
 
-  err = CurrentWindow.initGL()
-  if err != nil {
-    panic(err)
-  }
+	err = CurrentWindow.initGL()
+	if err != nil {
+		panic(err)
+	}
 
-  return CurrentWindow
+	return CurrentWindow
 }
 
 // Run starts the main game loop
 func (w *Window) Run() {
-  w.state.InitFunc()
-  last := time.Now()
-  for !w.GlfwWindow.ShouldClose() {
-    if w.state != nil {
-      elapsed := time.Since(last)
-      last = time.Now()
-      w.state.UpdateFunc(elapsed)
-      w.state.RenderFunc()
-    }
-    glfw.SwapInterval(1)
-    w.GlfwWindow.SwapBuffers()
-    glfw.PollEvents()
-  }
-  Exit()
+	w.state.InitFunc()
+	last := time.Now()
+	for !w.GlfwWindow.ShouldClose() {
+		if w.state != nil {
+			elapsed := time.Since(last)
+			last = time.Now()
+			w.state.UpdateFunc(elapsed)
+			w.state.RenderFunc()
+		}
+		glfw.SwapInterval(1)
+		w.GlfwWindow.SwapBuffers()
+		glfw.PollEvents()
+	}
+	CurrentWindow.state.CleanupFunc()
+	glfw.Terminate()
 }
 
 // Exit closes the game and cleans up
 func Exit() {
-  CurrentWindow.state.CleanupFunc()
-  CurrentWindow.GlfwWindow.SetShouldClose(true)
-  glfw.Terminate()
+	CurrentWindow.GlfwWindow.SetShouldClose(true)
 }
 
 func (w *Window) initGlfwWindow() (err error) {
-  var monitor *glfw.Monitor
+	var monitor *glfw.Monitor
 
-  if w.mode.Fullscreen {
-    monitor = glfw.GetPrimaryMonitor()
-  }
+	if w.mode.Fullscreen {
+		monitor = glfw.GetPrimaryMonitor()
+	}
 
-  CurrentWindow.GlfwWindow, err = glfw.CreateWindow(w.mode.Width, w.mode.Height, "", monitor, nil)
-  if err != nil {
-    return
-  }
+	CurrentWindow.GlfwWindow, err = glfw.CreateWindow(w.mode.Width, w.mode.Height, "", monitor, nil)
+	if err != nil {
+		return
+	}
 
-  w.GlfwWindow.MakeContextCurrent()
-  w.GlfwWindow.SetInputMode(glfw.CursorMode, glfw.CursorHidden)
+	w.GlfwWindow.MakeContextCurrent()
+	w.GlfwWindow.SetInputMode(glfw.CursorMode, glfw.CursorHidden)
 
-  return
+	return
 }
 
 func (w *Window) initGL() (err error) {
@@ -114,7 +113,7 @@ func (w *Window) initGL() (err error) {
 	gl.ClearColor(0.1, 0.1, 0.1, 0.0)
 	gl.MatrixMode(gl.PROJECTION)
 	gl.LoadIdentity()
-  PixelSize := 1
+	PixelSize := 1
 	gl.Ortho(0, float64(w.mode.Width/PixelSize), 0, float64(w.mode.Height/PixelSize), -1, 1)
 	var width, height = w.GlfwWindow.GetFramebufferSize()
 	fX, fY := int32(width/w.mode.Width), int32(height/w.mode.Height)
@@ -122,5 +121,5 @@ func (w *Window) initGL() (err error) {
 
 	gl.MatrixMode(gl.MODELVIEW)
 
-  return
+	return
 }
