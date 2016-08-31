16f76ba600985c334dc1ab7540c92b959d639106
mbuechmann <malte.buechmann@gmail.com>
2016-08-31 07:23:11 +0200

    Extract constant from keys onto single file

---
+++ keys/constants.go
@@ -0,0 +1,110 @@
+package keys
+
+import "github.com/go-gl/glfw/v3.1/glfw"
+
+const (
+	// Space is the space bar.
+	Space = Key(glfw.KeySpace)
+	// Up ist the up cursor.
+	Up = Key(glfw.KeyUp)
+	// Right ist the right cursor.
+	Right = Key(glfw.KeyRight)
+	// Down ist the down cursor.
+	Down = Key(glfw.KeyDown)
+	// Left ist the left cursor.
+	Left = Key(glfw.KeyLeft)
+	// Esc ist the escape key.
+	Esc = Key(glfw.KeyEscape)
+	// A key.
+	A = Key(glfw.KeyA)
+	// B key.
+	B = Key(glfw.KeyB)
+	// C key.
+	C = Key(glfw.KeyC)
+	// D key.
+	D = Key(glfw.KeyD)
+	// E key.
+	E = Key(glfw.KeyE)
+	// F key.
+	F = Key(glfw.KeyF)
+	// G key.
+	G = Key(glfw.KeyG)
+	// H key.
+	H = Key(glfw.KeyH)
+	// I key.
+	I = Key(glfw.KeyI)
+	// J key.
+	J = Key(glfw.KeyJ)
+	// K key.
+	K = Key(glfw.KeyK)
+	// L key.
+	L = Key(glfw.KeyL)
+	// M key.
+	M = Key(glfw.KeyM)
+	// N key.
+	N = Key(glfw.KeyN)
+	// O key.
+	O = Key(glfw.KeyO)
+	// P key.
+	P = Key(glfw.KeyP)
+	// Q key.
+	Q = Key(glfw.KeyQ)
+	// R key.
+	R = Key(glfw.KeyR)
+	// S key.
+	S = Key(glfw.KeyS)
+	// T key.
+	T = Key(glfw.KeyT)
+	// U key.
+	U = Key(glfw.KeyU)
+	// V key.
+	V = Key(glfw.KeyV)
+	// W key.
+	W = Key(glfw.KeyW)
+	// X key.
+	X = Key(glfw.KeyX)
+	// Y key.
+	Y = Key(glfw.KeyY)
+	// Z key.
+	Z = Key(glfw.KeyZ)
+	// N0 is the number Key 0.
+	N0 = Key(glfw.Key0)
+	// N1 is the number Key 1.
+	N1 = Key(glfw.Key1)
+	// N2 is the number Key 2.
+	N2 = Key(glfw.Key2)
+	// N3 is the number Key 3.
+	N3 = Key(glfw.Key3)
+	// N4 is the number Key 4.
+	N4 = Key(glfw.Key4)
+	// N5 is the number Key 5.
+	N5 = Key(glfw.Key5)
+	// N6 is the number Key 6.
+	N6 = Key(glfw.Key6)
+	// N7 is the number Key 7.
+	N7 = Key(glfw.Key7)
+	// N8 is the number Key 8.
+	N8 = Key(glfw.Key8)
+	// N9 is the number Key 9.
+	N9 = Key(glfw.Key9)
+	// LeftShift is the keft shift key.
+	LeftShift = Key(glfw.KeyLeftShift)
+	// LeftControl is the keft shift key.
+	LeftControl = Key(glfw.KeyLeftControl)
+	// LeftAlt is the keft shift key.
+	LeftAlt = Key(glfw.KeyLeftAlt)
+	// LeftSuper is the keft shift key.
+	LeftSuper = Key(glfw.KeyLeftSuper)
+	// RightShift is the keft shift key.
+	RightShift = Key(glfw.KeyRightShift)
+	// RightControl is the keft shift key.
+	RightControl = Key(glfw.KeyRightControl)
+	// RightAlt is the keft shift key.
+	RightAlt = Key(glfw.KeyRightAlt)
+	// RightSuper is the keft shift key.
+	RightSuper = Key(glfw.KeyRightSuper)
+	// Menu Key.
+	Menu = Key(glfw.KeyMenu)
+	// Last Key.
+	Last = Key(glfw.KeyLast)
+)
