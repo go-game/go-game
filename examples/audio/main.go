41f932a04f3a51a47b9ea519ee9b0a507190d428
mbuechmann <malte.buechmann@gmail.com>
2016-09-01 11:06:51 +0200

    Improve audo example

--- examples/audio/main.go
+++ examples/audio/main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"time"
+
 	"git.mbuechmann.com/go-game/audio"
-	"github.com/vova616/go-openal/openal"
+	"git.mbuechmann.com/go-game/desktop"
+	"git.mbuechmann.com/go-game/game"
+	"git.mbuechmann.com/go-game/gfx"
+	"git.mbuechmann.com/go-game/keys"
 )
 
+var source *audio.Source
+
 func main() {
+	gameState := &game.State{
+		InitFunc:   initGame,
+		RenderFunc: render,
+		UpdateFunc: logic,
+		OnKeyDown:  onKeyDown,
+	}
+
+	mode := &desktop.Mode{Width: 1280, Height: 800, Fullscreen: false}
+	desktop.OpenWindow(mode)
+	gfx.SetPixelSize(4)
+
+	desktop.Run(gameState)
+
+}
+
+func initGame() {
 	data, err := audio.LoadData("assets/welcome.wav")
 	if err != nil {
 		panic(err)
 	}
 
-	source := data.NewSource()
+	source = data.NewSource()
 
 	source.Play()
-	for source.State() == openal.Playing {
-	}
 	return
+
+}
+
+func render() {
+	gfx.Clear()
+}
+
+func onKeyDown(k keys.Key) {
+	if k == keys.Esc {
+		desktop.Exit()
+	}
+}
+
+func logic(delta time.Duration) {
+
 }
