02b4fdbbb58dcd7b4344d2c1d2b75c8e07b72b71
mbuechmann <malte.buechmann@gmail.com>
2016-09-01 11:05:53 +0200

    Add more functions to audio source

--- audio/source.go
+++ audio/source.go
@@ -28,3 +28,28 @@ func (s *Source) Stop() {
 func (s *Source) State() int32 {
 	return s.openalSource.State()
 }
+
+// SetPitch sets the pitch
+func (s *Source) SetPitch(f float32) {
+	s.openalSource.SetPitch(f)
+}
+
+// SetGain sets the gain
+func (s *Source) SetGain(f float32) {
+	s.openalSource.SetGain(f)
+}
+
+// SetPosition sets the position
+func (s *Source) SetPosition(x, y, z float32) {
+	s.openalSource.SetPosition(x, y, z)
+}
+
+// SetVelocity sets the velocity
+func (s *Source) SetVelocity(x, y, z float32) {
+	s.openalSource.SetVelocity(x, y, z)
+}
+
+// SetLooping sets if the source is looping
+func (s *Source) SetLooping(b bool) {
+	s.openalSource.SetLooping(b)
+}
