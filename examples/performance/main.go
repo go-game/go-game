944c612e695a98717da25e84f39ea669f7d44309
mbuechmann <malte.buechmann@gmail.com>
2016-09-28 17:35:50 +0200

    Implement performance example

--- examples/performance/main.go
+++ examples/performance/main.go
@@ -3,23 +3,33 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"git.mbuechmann.com/go-game/desktop"
 	"git.mbuechmann.com/go-game/game"
 	"git.mbuechmann.com/go-game/gfx"
 	"git.mbuechmann.com/go-game/keys"
 	"git.mbuechmann.com/go-game/mouse"
-	"time"
 )
 
 type heart struct {
 	RenderOptions *gfx.RenderOptions
+	Vx, Vy        float64
 }
+
+const (
+	WIDTH  = 1280
+	HEIGHT = 800
+	G      = 600 / float64(time.Second*time.Second)
+)
 
 var hearts []*heart
 var image *gfx.Image
+var adding bool
 
 func main() {
-	mode := &desktop.Mode{Width: 1280, Height: 800, Fullscreen: false}
+	mode := &desktop.Mode{Width: WIDTH, Height: HEIGHT, Fullscreen: false}
 	desktop.OpenWindow(mode)
 
 	desktop.Run(&game.State{
@@ -29,6 +39,7 @@ func main() {
 		RenderFunc:        render,
 		OnKeyDown:         onKeyPress,
 		OnMouseButtonDown: onMouseButtonDown,
+		OnMouseButtonUp:   onMouseButtonUp,
 	})
 }
 
@@ -42,14 +53,53 @@ func cleanup() {
 
 func render() {
 	gfx.Clear()
+	for _, h := range hearts {
+		gfx.Render(image, h.RenderOptions)
+	}
 }
 
 func update(delta time.Duration) {
+	if adding {
+		vx := (rand.Float64()*400 + 100) / float64(time.Second)
+		vy := (rand.Float64()*100 + 100) / float64(time.Second)
+		h := heart{RenderOptions: gfx.NewRenderOptions(), Vx: vx, Vy: vy}
+		hearts = append(hearts, &h)
+	}
 
+	for _, h := range hearts {
+		h.RenderOptions.X += h.Vx * float64(delta)
+		if h.Vx > 0 {
+			d := WIDTH - h.RenderOptions.X - float64(image.Width())
+			if d < 0 {
+				h.RenderOptions.X -= d
+				h.Vx *= -1
+			}
+		} else {
+			if h.RenderOptions.X < 0 {
+				h.RenderOptions.X *= -1
+				h.Vx *= -1
+			}
+		}
+
+		h.Vy += G * float64(delta)
+		h.RenderOptions.Y += h.Vy * float64(delta)
+		// h.Vy = math.Min(h.Vy, VCAP)
+		if h.Vy > 0 {
+			d := HEIGHT - h.RenderOptions.Y - float64(image.Height())
+			if d < 0 {
+				h.RenderOptions.Y -= d
+				h.Vy *= -1
+			}
+		}
+	}
+}
+
+func onMouseButtonUp(b mouse.Button, x, y float32) {
+	adding = false
 }
 
 func onMouseButtonDown(b mouse.Button, x, y float32) {
-
+	adding = true
 }
 
 func onKeyPress(k keys.Key) {
